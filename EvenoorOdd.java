fb66b40b491988068fb430f4c502e28aa75f42ea
shivammiet <shivamwalther@gmail.com>
2023-02-07 10:43:16 +0530

    initial commit

---
+++ EvenoorOdd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+public class EvenoorOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x%2==0)
+            System.out.println("Even");
+            else
+                System.out.println("odd");
+            }
+        }
