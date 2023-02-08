f4de5c7fcb28071eb243e2dba024ae8d11f404cc
shivammiet <shivamwalther@gmail.com>
2023-02-08 23:25:32 +0530

    initial commit

---
+++ harshadnno.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+public class harshadnno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the elemennt");
+        int n = sc.nextInt();
+         
+    }
+
+    
+}
