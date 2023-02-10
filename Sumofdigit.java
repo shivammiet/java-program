64a25ddeb7016e8cc25cda93901dffc7bf23195b
shivammiet <shivamwalther@gmail.com>
2023-02-10 23:33:30 +0530

    initial.commit

--- Sumofdigit.java
+++ Sumofdigit.java
@@ -1,26 +1,19 @@
 import java.util.*;
 
-import javax.swing.plaf.synth.SynthSpinnerUI;
-public class harshadnno {
+import javax.swing.plaf.synth.SynthDesktopIconUI;
+public class Sumofdigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the elemennt");
         int n = sc.nextInt(), r, sum=0;
         while(n!=0){
             r = n%10;
             sum = sum + r;
             n = n/10;
+            
         }
         System.out.println(sum);
-         if(n%sum==0)
-            System.out.println("Harshad number");
-        else
-        System.out.println("not harshad no");
 
 
     }
+    
 }
-    
-
-    
-
