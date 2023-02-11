49df8f80e824a19e9e3d9018634c74e3555b492f
shivammiet <shivamwalther@gmail.com>
2023-02-11 22:29:52 +0530

    initial.commit
    while condition

---
+++ whilecondition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import javax.sql.rowset.spi.SyncResolver;
+import javax.swing.plaf.synth.SynthDesktopIconUI;
+
+public class whilecondition {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){
+  
+        System.out.println(i);
+        i=i+1;
+    }
+    System.out.println("done");
+    
+}
+}
