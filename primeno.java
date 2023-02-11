b889c1c443e04953bc0cd8a63c663a0bb8b6aa5c
shivammiet <shivamwalther@gmail.com>
2023-02-12 02:25:07 +0530

    initial.commit
    prime number

---
+++ primeno.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class primeno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i =0; i <t; i++){
+            int n = sc.nextInt();
+            int count = 0;
+            for(int div = 1; div <=n; div++){
+                if(n%div==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.println("prime no");
+            }else
+            System.out.println("not prime no");
+        }
+    }
+    
+}
