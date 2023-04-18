c65dfd662b9cd1cf476109b61da9f6ad4343994e
shivammiet <shivamwalther@gmail.com>
2023-04-18 23:31:47 +0530

    initial.commit

---
+++ leapyear.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class leapyear {
+    public static void main(String[] args) {
+     int year = 2020;
+
+     if (year % 400 == 0)
+       System.out.println (year + " is a Leap Year");
+
+     else if (year % 4 == 0 && year % 100 != 0)
+       System.out.println (year + " is a Leap Year");
+
+     else
+         System.out.println (year + " is not a Leap Year");
+
+   }
+ }
+
